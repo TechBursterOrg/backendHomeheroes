28ef0a53cf11d742d02366af83bbb62333f421fc
PeterFreemann <128150016+PeterFreemann@users.noreply.github.com>
2025-11-08 00:19:40 +0100

    corsproblem

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,10 +8,34 @@ import User from '../models/User.js';
 import VerificationToken from '../models/VerificationToken.js';
 import smsService from '../utils/smsService.js';
 import { sendVerificationEmail } from '../utils/emailService.js';
+import cors from 'cors';
+
 
 
 
 const router = express.Router();
+
+router.use(cors({
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      'https://homeheroes.help',
+      'https://www.homeheroes.help', 
+      'http://localhost:5173',
+      'http://localhost:5174'
+    ];
+    
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'), false);
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin']
+}));
+
+
 
 // Authentication middleware
 function authenticateToken(req, res, next) {
